e79c2db264c2c8c0e23c9d8a5e2590467922cd96
Jordan Rossen <jordanerossen@gmail.com>
2025-02-21 14:58:55 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-multisusie_directory="$(dirname "$(readlink -f "$0")")"
-python -m venv ${multisusie_directory}/multisusie_env
-${multisusie_directory}/multisusie_env/bin/pip install -r ${multisusie_directory}/requirements.txt
-${multisusie_directory}/multisusie_env/bin/pip install ${multisusie_directory} -U
-${multisusie_directory}/multisusie_env/bin/python ${multisusie_directory}/examples/example_for_install_script.py ${multisusie_directory}
+python -m venv multisusie_env
+MultiSuSiE/multisusie_env/bin/pip install -r MultiSuSiE/requirements.txt
+MultiSuSiE//multisusie_env/bin/pip install MultiSuSiE/ -U
+MultiSuSiE//multisusie_env/bin/python MultiSuSiE//examples/example_for_install_script.py
