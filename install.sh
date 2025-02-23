19c1cdea0c9ad6a61467b8de1a3a5029b440aa3a
Jordan Rossen <jordanerossen@gmail.com>
2025-02-23 18:08:21 -0500

    changed install script to not create a virtual environment.

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-python -m venv multisusie_env
-MultiSuSiE/multisusie_env/bin/pip install -r MultiSuSiE/requirements.txt
-MultiSuSiE//multisusie_env/bin/pip install MultiSuSiE/ -U
-MultiSuSiE//multisusie_env/bin/python MultiSuSiE//examples/example_for_install_script.py
+pip install -r MultiSuSiE/requirements.txt
+pip install MultiSuSiE/ -U
+python MultiSuSiE//examples/example_for_install_script.py
